627545ac5833913fcf5f4474ff39e1be450dd655
Pernell Styles Jr <82741858+Stylesp007@users.noreply.github.com>
2023-01-12 01:34:20 -0500

    created more components
    Components to house the conversation modal and the contact modal

--- src/Componets/Sidebar.js
+++ src/Componets/Sidebar.js
@@ -1,17 +1,21 @@
 import React, {useState}from 'react'
-import { Tab, Nav } from 'react-bootstrap'
+import { Tab, Nav, Button, Modal } from 'react-bootstrap'
 import Conversations from './Conversations'
 import Contacts from './Contacts'
+import NewContactModal from './NewContactModal'
+import NewConversationModal from './NewConversationModal'
+
 
 const CONVERSATIONS_KEY = 'conversations'
 const CONTACTS_KEY = 'contacts'
 
 export default function Sidebar({ id }) {
   const [activeKey, setActiveKey] = useState(CONVERSATIONS_KEY)
+  const conversationOpen = activeKey === CONVERSATIONS_KEY
 
   return (
     <div style={{ width: '250px'}} className="d-flex flex-column">
-      <Tab.Container activeKey={activeKey} onselect={setActiveKey}>
+      <Tab.Container activeKey={activeKey} onSelect={setActiveKey}>
         <Nav variant='tabs' className='justify-content-center' >
           <Nav.Item>
             <Nav.Link eventKey= {CONVERSATIONS_KEY}>Conversations</Nav.Link>
@@ -20,7 +24,7 @@ export default function Sidebar({ id }) {
             <Nav.Link eventKey = {CONTACTS_KEY}>Contacts</Nav.Link>
           </Nav.Item>
         </Nav>
-        <Tab.Content>
+        <Tab.Content className="border-end overflow-auto flex-grow-1">
           <Tab.Pane eventKey={CONVERSATIONS_KEY}>
             <Conversations />
           </Tab.Pane>
@@ -28,7 +32,20 @@ export default function Sidebar({ id }) {
             <Contacts />
           </Tab.Pane>
         </Tab.Content>
+        <div className="p-2 border-top border-end small">
+          Your Id: <span className='text-muted'>{id}</span>
+        </div>
+        <Button className='rounded-0' >
+          New {conversationOpen ? 'Conversation' : 'Contact'}
+        </Button>
       </Tab.Container>
+
+      <Modal>
+        {conversationOpen ?
+          <NewConversationModal /> :
+          <NewContactModal />
+        }
+      </Modal>
     </div>
   )
 }
