2b521664bacca90fbbc4bea8813c71003c4deed7
Pernell Styles Jr <82741858+Stylesp007@users.noreply.github.com>
2023-01-11 23:52:15 -0500

    created more components

--- src/Componets/Sidebar.js
+++ src/Componets/Sidebar.js
@@ -1,15 +1,33 @@
-import React from 'react'
+import React, {useState}from 'react'
 import { Tab, Nav } from 'react-bootstrap'
+import Conversations from './Conversations'
+import Contacts from './Contacts'
+
+const CONVERSATIONS_KEY = 'conversations'
+const CONTACTS_KEY = 'contacts'
+
 export default function Sidebar({ id }) {
+  const [activeKey, setActiveKey] = useState(CONVERSATIONS_KEY)
+
   return (
     <div style={{ width: '250px'}} className="d-flex flex-column">
-      <Tab.Container>
-        <Nav.Item>
-          <Nav.Link eventKey= "conversations">Conversations</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey = "contacts">Contacts</Nav.Link>
-        </Nav.Item>
+      <Tab.Container activeKey={activeKey} onselect={setActiveKey}>
+        <Nav variant='tabs' className='justify-content-center' >
+          <Nav.Item>
+            <Nav.Link eventKey= {CONVERSATIONS_KEY}>Conversations</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey = {CONTACTS_KEY}>Contacts</Nav.Link>
+          </Nav.Item>
+        </Nav>
+        <Tab.Content>
+          <Tab.Pane eventKey={CONVERSATIONS_KEY}>
+            <Conversations />
+          </Tab.Pane>
+          <Tab.Pane eventKey={CONTACTS_KEY}>
+            <Contacts />
+          </Tab.Pane>
+        </Tab.Content>
       </Tab.Container>
     </div>
   )
