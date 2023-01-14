0c7be78e51f3e4bd8b096993f00963b5eb7f62aa
Pernell Styles Jr <82741858+Stylesp007@users.noreply.github.com>
2023-01-14 09:58:05 -0500

    contacts are stored and able to called On

--- src/Componets/Conversations.js
+++ src/Componets/Conversations.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import { ListGroup } from 'react-bootstrap'
+import { useConversations } from '../contexts/ConversationsProvider'
 
 export default function Conversations() {
+  const { conversations, selectedConversationIndex} = useConversations()
   
   return (
-    <div>
-      Conversations
-    </div>
+    <ListGroup variant="flush">
+      {conversations.map((conversation, index) => (
+        <ListGroup.Item 
+          key={index}
+          action
+          onClick={() => selectedConversationIndex(index)}
+          active={conversation.selected}>
+
+          {conversation.recipients.map(recipients => recipients.name).join(',')}
+        </ListGroup.Item>
+      ))}
+    </ListGroup>
   )
 }
--- src/contexts/ContactsProvider.js
+++ src/contexts/ContactsProvider.js
@@ -1,6 +1,7 @@
 import React,{ useContext } from 'react'
 import useLocalStorage from '../hooks/useLocalStorage'
 
+
 const ContactsContext = React.createContext()
 
 export function useContacts(){
@@ -9,12 +10,15 @@ export function useContacts(){
 
 export function ContactsProvider({ children }) {
   const [contacts, setContacts] = useLocalStorage('contacts', [])
+  
 
   function createContact(id, name){
     setContacts(prevContacts => {
       return[...prevContacts, {id, name }]
     })
   }
+
+
   return (
     <ContactsContext.Provider value={{ contacts, createContact}}>
       {children}
